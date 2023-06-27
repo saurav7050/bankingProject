bfe3751690b8fc43dd70de561af3eaaa906eea02
saurav kumar <saurav2secb@gmail.com>
2023-06-27 13:30:58 +0530

    Add files via upload

---
+++ bankAppli.java
@@ -0,0 +1,11 @@
+package com.bankingProject;
+
+public class bankAppli {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankingApplication b=new BankingApplication();
+		b.start();
+	}
+
+}
